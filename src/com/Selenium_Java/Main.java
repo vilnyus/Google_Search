6a30aacab2deb94e210f0591a2df29c572737457
vagha <govagharsh@gmail.com>
2021-04-28 14:53:01 +0400

    Finnal commit TImeout is not working

--- src/com/Selenium_Java/Main.java
+++ src/com/Selenium_Java/Main.java
@@ -4,16 +4,43 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
+
 
 public class Main {
 
     public static void main(String[] args) {
+        String currentUrl = "https://demoqa.com/buttons";
+        String linksPageUrl = "https://demoqa.com/links";
+        String newUrl;
         System.setProperty("webdriver.chrome.driver", "src\\drivers\\chromedriver.exe");
         WebDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get("http://www.google.com/");
-        WebElement element = chromeDriver.findElement(By.name("q"));
-        element.sendKeys("Hello Selenium WebDriver.");
-        element.submit();
-        System.out.println("Page title is .." + chromeDriver.getTitle());
+
+        chromeDriver.get(currentUrl);
+        WebElement doubleClickButton = chromeDriver.findElement(By.id("doubleClickBtn"));
+        WebElement rightClickButton = chromeDriver.findElement(By.id("rightClickBtn"));
+
+        Actions a = new Actions(chromeDriver);
+
+        a.moveToElement(doubleClickButton).doubleClick().build().perform();
+        chromeDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        a.moveToElement(rightClickButton).contextClick().build().perform();
+        chromeDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        WebElement doubleClickText = chromeDriver.findElement(By.id("doubleClickMessage"));
+        WebElement rightClickText = chromeDriver.findElement(By.id("rightClickMessage"));
+        boolean isDisplayed = doubleClickText.isDisplayed() || rightClickText.isDisplayed();
+        System.out.println("Button is double clicked and right clicked " + isDisplayed);
+
+        chromeDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        chromeDriver.get(linksPageUrl);
+        newUrl = chromeDriver.findElement(By.linkText("Home")).getAttribute("href");
+        chromeDriver.navigate().to(newUrl);
+
+
+
     }
 }
